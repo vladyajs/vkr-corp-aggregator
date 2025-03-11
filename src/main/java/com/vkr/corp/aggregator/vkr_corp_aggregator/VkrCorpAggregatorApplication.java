4ff7d98ab963026405fe00190cd3459e19a46b01
Владислав Андреевич Шипай <shipay@atb.su>
2025-03-11 20:15:22 +0300

    initial commit

---
+++ src/main/java/com/vkr/corp/aggregator/vkr_corp_aggregator/VkrCorpAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.vkr.corp.aggregator.vkr_corp_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkrCorpAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkrCorpAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vkr/corp/aggregator/vkr_corp_aggregator/VkrCorpAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkr.corp.aggregator.vkr_corp_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkrCorpAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
